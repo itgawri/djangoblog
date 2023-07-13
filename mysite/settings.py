08bd30d9fdd9a3f2241dccba1afaf0ab99feaede
19gaw <19itgawri97@gmail.com>
2023-07-13 13:33:03 +0200

    zmienilem debug na true, dodalem kod hosta 127.0.0.1, oraz nzwe poprawna strony

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6!_^be*ub%dushp#1uyp@ug!9xotrg-a%@#pktq)16*z2sv=p@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['itgawri.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'itgawri.pythonanywhere.com']
 
 
 # Application definition
