a50f27a4fc515c7367bac8d818598bf3175e3966
19gaw <19itgawri97@gmail.com>
2023-08-08 12:54:40 +0200

    dodanie new post

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
-from django.forms import ImgForm
+from django.forms import PostForm
 from django.views.generic import DetailView
 from django.views.generic import TemplateView
 
 class Image(TemplateView):
-    form = ImgForm
+    form = PostForm
     template_name = 'blog/image.html'
 
     def post(self, request, *args, **kwargs):
-        form = ImgForm(request.POST, request.FILES)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             obj = form.save()
             return HttpResponseRedirect(reverse_lazy('image_display', kwargs={'pk':obj.id}))
@@ -41,3 +41,16 @@ def post_detail(request, pk):
 def error_404_view(request, exception):
     data = {"name": 'Blog dla programistów'}
     return render(request, 'blog/404.html', data)
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
